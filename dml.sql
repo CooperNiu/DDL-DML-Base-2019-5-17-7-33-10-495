ffbb99b188cbcf5071f0b391a5a3a180aafff2ea
CooperNiu <niuyanno.1@outlook.com>
2019-07-10 17:57:45 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- 插入记录  
-
+insert into student values()
 -- 修改记录  
-
+update student 
+set name = 'mike'
+where id = 13
 --  删除记录  
-
+delete from student
+where id = 13
 -- 查询记录  
+select * from student;
